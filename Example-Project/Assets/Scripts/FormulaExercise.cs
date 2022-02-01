1ac60e27d038bc87060d72cffeb2941c566873ad
Nhlanhla <nhlanhla.pandeka@go4ir.com>
2022-02-01 10:43:33 +0200

    Completed question1 code

---
+++ Example-Project/Assets/Scripts/FormulaExercise.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormulaExercise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Question1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Question1()
+    {
+        /*
+         Consider a car of mass 1 000 kg with a velocity of 8 ms 
+        (about 30 kmh) East. The momentum of the car is therefore.
+
+        Formula: p = mv
+         */
+
+        float p, m = 1000, v = 8;
+        p = m * v;
+        print($"p = {p} kgm/s");
+    }
+}
