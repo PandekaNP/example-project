51c91d1b4f4e59206e3861737d677100b4c9155f
Nhlanhla <nhlanhla.pandeka@go4ir.com>
2022-02-01 11:45:27 +0200

    Completed question 2

--- Example-Project/Assets/Scripts/FormulaExercise.cs
+++ Example-Project/Assets/Scripts/FormulaExercise.cs
@@ -7,7 +7,7 @@ public class FormulaExercise : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Question1();
+        Question2();
     }
 
     // Update is called once per frame
@@ -27,5 +27,24 @@ public class FormulaExercise : MonoBehaviour
         float p, m = 1000, v = 8;
         p = m * v;
         print($"p = {p} kgm/s");
+    }
+
+    void Question2()
+    {
+        /*
+         A 0.170kg pool ball travelling 6 ms hits 
+        another 0.170kg ball at rest.  If the first ball stops 
+        after the collision, how fast is the second ball moving?
+        vf2?
+        formula: m1v1i + m2v2i = m1v1f + m2v2f
+         */
+
+        float m1=0.17f, vi1=6, m2 = 0.17f, vi2=0, vf1=0, vf2;
+        float m1vi1 = m1 * vi1;
+        float m2vi2 = m2 * vi2;
+        float m1vf2 = m1 * vf1;
+        vf2 = (m1vi1 + m2vi2) / m2;
+
+        print($"final velocity = {vf2} ms");
     }
 }
